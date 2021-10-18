ab345c42352c786a655e45b43b22e86c378ff159
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-10-18 09:17:58 +0200

    Add files via upload

---
+++ PrincipiobjektnoorijentiranogprogramiranjaZadatak3/PrincipiobjektnoorijentiranogprogramiranjaZadatak3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiobjektnoorijentiranogprogramiranjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brod brod = new Brod();
+            brod.KudaVozi();
+
+            Zrakoplov zrakoplov = new Zrakoplov();
+            zrakoplov.KudaVozi();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrincipiobjektnoorijentiranogprogramiranjaZadatak3/PrincipiobjektnoorijentiranogprogramiranjaZadatak3/Vozilo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiobjektnoorijentiranogprogramiranjaZadatak3
+{
+    class Vozilo
+    {
+        public bool JeLiLeti;
+        public bool JeLiPlovi;
+
+        public void KudaVozi()
+        {
+            Console.WriteLine("{0} {1}", JeLiLeti, JeLiPlovi);
+        }
+    }
+}
